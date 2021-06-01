34dbd96cc3707d69f02d07a7a006b6fdca5a73ac
Bruno Trindade <brunotrindade@MacBook-Pro.local>
2021-06-01 17:26:40 -0300

    :construction: - delete modality

--- app/Controllers/Http/ModalityController.js
+++ app/Controllers/Http/ModalityController.js
@@ -15,6 +15,12 @@ class ModalityController {
     const modalities = await Modality.all();
     return response.json(modalities);
   }
+  async delete({ request, response, auth, params }) {
+    const relation = await Database.table("modalities")
+      .where({ id: params.id })
+      .delete();
+    return response.status(200).json(relation);
+  }
 }
 
 module.exports = ModalityController;
